6122589b296c8ecd46d2dfab7e624217eded4fbe
d3QUone <volkvid@yandex.ru>
2015-02-07 10:54:38 +0300

    tasks 1,2,3 display not added yet

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,101 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Владимир on 06.02.15.
+//  Copyright (c) 2015 Kasatkin. All rights reserved.
+//
+
+import Foundation
+
+
+class CalculatorBrain {
+    
+    private enum Op: Printable {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand): return "\(operand)"
+                case .UnaryOperation(let symbol, _): return symbol
+                case .BinaryOperation(let symbol, _): return symbol
+                }
+            }
+        }
+    }
+    
+    private var opStack = [Op]()
+    
+    private var knownOps = [String: Op]()
+    
+    init(){
+        func learnOp(op: Op){
+            knownOps[op.description] = op
+        }
+        learnOp(Op.BinaryOperation("×", *))
+        learnOp(Op.BinaryOperation("÷") { $1 / $0 })
+        learnOp(Op.BinaryOperation("+", +))
+        learnOp(Op.BinaryOperation("−") { $1 - $0 })
+        learnOp(Op.BinaryOperation("^", pow))
+        learnOp(Op.UnaryOperation("√", sqrt))
+        learnOp(Op.UnaryOperation("sin", sin))
+        learnOp(Op.UnaryOperation("cos", cos))
+        learnOp(Op.UnaryOperation("tan", tan))
+        learnOp(Op.UnaryOperation("+/−") { -1*$0 })
+        
+        
+        //knownOps["×"] = Op.BinaryOperation("×", *)
+        //knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
+    }
+    
+    private func evaulate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaulate = evaulate(remainingOps)
+                if let operand = operandEvaulate.result {
+                    return (operation(operand), operandEvaulate.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Eval = evaulate(remainingOps)
+                if let op1result = op1Eval.result {
+                    let op2Eval = evaulate(op1Eval.remainingOps)
+                    if let op2result = op2Eval.result {
+                        return (operation(op1result, op2result), op2Eval.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func evaulate() -> Double? {
+        let (result, remainder) = evaulate(opStack)
+        //println("\(opStack) = \(result!) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaulate()
+    }
+    
+    func performOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol]{
+            opStack.append(operation)
+        }
+        return evaulate()
+    }
+    
+    func clearStack() {
+        opStack.removeAll(keepCapacity: false)
+        println("opStack clear")
+    }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Владимир on 06.02.15.
+//  Copyright (c) 2015 Kasatkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
+    var isDisplayUsing = false
+    var dotWasTypedIn = false
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func digit(sender: UIButton) {
+        var digit: String
+        if sender.currentTitle! == "π" {
+            digit = "\(M_PI)"
+        }
+        else {
+            digit = sender.currentTitle!
+        }
+        
+        if isDisplayUsing {
+            if digit == "." {
+                if !dotWasTypedIn {
+                    dotWasTypedIn = true
+                    display.text = display.text! + digit
+                }
+                else {
+                    println("wrong extra dot")
+                }
+            } else {
+                display.text = display.text! + digit
+            }
+        }
+        else {
+            if digit != "." {
+                isDisplayUsing = true
+                display.text = digit
+            }
+            else {
+                println("you can't start from dot")
+            }
+        }
+    }
+    
+    @IBAction func operation(sender: UIButton) {
+        if isDisplayUsing {
+            enter()
+        }
+        if let operation  = sender.currentTitle {
+            if let result = brain.performOperation(operation){
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func enter() {
+        isDisplayUsing = false
+        dotWasTypedIn = false
+        println("current displayValue: \(displayValue)")
+        if let result = brain.pushOperand(displayValue){
+            displayValue = result
+        } else {
+            // tell that something wrong 
+            displayValue = 0
+        }
+    }
+    
+    @IBAction func clear() {
+        display.text = "0"
+        isDisplayUsing = false
+        dotWasTypedIn = false
+        brain.clearStack()
+    }
+    
+    @IBAction func backspace() {
+        let buf = display.text!
+        if countElements(buf) > 1 {
+            display.text = buf.substringToIndex(advance(buf.startIndex, countElements(buf)-1))
+        }
+        else if countElements(buf) == 1 {
+            display.text = "0"
+            isDisplayUsing = false
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+        }
+    }
+}
+
